04dcfeeb67a333ab07ff08e516039e5a00d55f3b
Никита Ткаченко <nike173145@gmail.com>
2023-05-17 13:03:48 +0300

    gem install commit

---
+++ lib/diff.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "diff/version"
+
+module Diff
+  class Error < StandardError; end
+
+  def self.hello
+    "hello world"
+  end
+end
---
+++ lib/diff/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Diff
+  VERSION = "0.1.0"
+end
