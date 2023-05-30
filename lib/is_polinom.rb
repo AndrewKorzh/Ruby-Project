d80382ce97f5fcf45506fd2079570efcc67aac97
AndrewKorzh <>
2023-05-30 15:58:25 +0300

    Tests And Change Func

--- lib/is_polinom.rb
+++ lib/is_polinom.rb
@@ -1,27 +1,3 @@
-def check_polin(p)
-  s = "+-*^"
-  # Проверка подряд идущих знаков
-  s.each_char do |c1|
-    s.each_char do |c2|
-      if p.include?("#{c1}#{c2}")
-        return false
-      end
-    end
-  end
-
-  (p.length - 1).times do |i|
-    # 5x - no
-    if p[i].is_a?(Numeric) && is_letter?(p[i + 1])
-      return false
-    end
-    # x5 - no
-    if is_letter?(p[i]) && p[i + 1].is_a?(Numeric)
-      return false
-    end
-  end
-  return true
-end
-
 def is_number?(s)
   s =~ /[0-9]/
 end
@@ -40,9 +16,7 @@ def is_polinom?(p)
     if is_number?(p[i]) && is_letter?(p[i+1])
       return false
     end
-    if is_letter?(p[i]) && p[i+1]!='^' && (ss.include? p[i+1] == false)
-      return false
-    end
+
   end
   (p.length - 2).times do |i|
     if is_letter?(p[i]) && p[i+1] == '*' && is_letter?(p[i+2])
@@ -63,4 +37,40 @@ def is_polinom?(p)
     i += 1
   end
   return true
+end
+
+s2 = []
+s2.push("x^2+5*x-4")
+s2.push("x^2+5*x+5")
+s2.push("x-2")
+s2.push("4*x+10*x^2-134")
+s2.push("x+3-y")
+s2.push("2x")
+s2.push("9xi")
+s2.push("x^q-5")
+s2.push("-")
+
+s2.push("x^2+")
+s2.push("x^2+5**x+5")
+s2.push("x-2^2")
+s2.push("4*x+10*x^2-13[4]")
+s2.push("x+3-y-")
+s2.push("2x2")
+s2.push("aboba")
+s2.push("x^q-5")
+s2.push("")
+
+s2.push("x^222222+22222*x^55555")
+s2.push("x^2+5*x+5+y^222-z-x")
+s2.push("x-2+x+x+x+x-x-x")
+s2.push("4*x+10*x^2-13")
+s2.push("x+3-y-xyz")
+s2.push("2*x+2*ccccc")
+s2.push("aboba")
+s2.push("x^q-5")
+s2.push("")
+
+(s2.length).times do |i|
+  p s2[i]
+  p is_polinom?(s2[i])
 end
